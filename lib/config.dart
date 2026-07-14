45cb92e8cd5040add3a7cd2bbbc23a21242e09bc
proDevAnant <anantd6911@gmail.com>
2026-07-14 13:24:20 +0530

    Set live backend URL

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,4 @@
 class AppConfig {
-  // ==== YAHAN apne PHP server ka URL daalein ====
-  // Android emulator + local XAMPP:  http://10.0.2.2/chatapp_api
-  // Real phone (same wifi):          http://192.168.x.x/chatapp_api
-  // Hosting par:                     https://yourdomain.com/chatapp_api
-  static const String baseUrl = 'http://10.0.2.2/chatapp_api';
+  static const String baseUrl =
+      'https://lightseagreen-dunlin-977553.hostingersite.com';
 }
